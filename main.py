87a7eac441ae2e7f76bbdbc267caad9bb2740273
Sibghatullah28 <m.sibghatullah28@gmail.com>
2026-06-28 09:36:46 +0200

    Updated FastAPI with Hugging Face client code

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from fastapi import FastAPI
+import io
+
+from fastapi import FastAPI, HTTPException
 import feedparser
+from fastapi.responses import Response
+from pydantic import BaseModel
+from huggingface_hub import InferenceClient
 
 app = FastAPI(
     title="Trend Service",
@@ -7,6 +12,34 @@ app = FastAPI(
     description="AI Trend Collection Service"
 )
 
+HF_Token="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"
+
+client = InferenceClient(
+    provider="fal-ai",
+    api_key=HF_Token,
+)
+class ImageRequest(BaseModel):
+    prompt: str
+
+@app.post("/generate-image")
+async def generate_image(request: ImageRequest):
+    try:
+        # Hugging Face se FLUX.1-dev model ke zariye image generate karwana
+        image = client.text_to_image(
+            request.prompt,
+            model="black-forest-labs/FLUX.1-dev",
+        )
+        
+        # Image ko Bytes mein convert karna taake n8n isay as a file receive kar sake
+        img_byte_arr = io.BytesIO()
+        image.save(img_byte_arr, format='PNG')
+        img_byte_arr = img_byte_arr.getvalue()
+        
+        # Direct raw image binary wapis bhejna
+        return Response(content=img_byte_arr, media_type="image/png")
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Hugging Face Error: {str(e)}")    
 
 @app.get("/trends")
 def get_trends():
